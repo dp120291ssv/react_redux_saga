4ff8af3482289b8787426c2e7d231b9b5db201a1
Semenov Sergei <dp120291ssv@gmail.com>
2022-10-21 16:10:17 +0300

    add store

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import {increaseCount, decreaseCount} from "./redux/actions/actionCreator";
+import store from "./redux/store";
+
+// https://react-redux.js.org/using-react-redux/usage-with-typescript#typing-the-useselector-hook
+type StoreProps = {
+    counter: {count: number}
+}
 
 const App = () => {
+    // const store = useSelector(store => store);
+    // console.log('store', store)
+    const count = useSelector((store: StoreProps)  => store?.counter?.count || 0);
+    const dispatch = useDispatch();
+    // const [count, setCount] = useState(0)
+
+    console.log('store', store)
+
+    const handleIncrease = () => {
+        // setCount(count + 1)
+        dispatch(increaseCount())
+    }
+
+    const handleDecrease = () => {
+        // setCount(count - 1)
+        dispatch(decreaseCount())
+    }
+
     return (
         <div>
-          <h1>HELLO REDUX-SAGA</h1>
+            <h1>HELLO REDUX-SAGA</h1>
+            <h1>Count: {count}</h1>
+            <button onClick={handleIncrease}>Increase</button>
+            <button onClick={handleDecrease}>Decrease</button>
         </div>
     );
 };
