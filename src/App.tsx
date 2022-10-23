66dd30f9896cf74725c9fec0240a6833fd6db5b3
Semenov Sergei <dp120291ssv@gmail.com>
2022-10-23 21:09:55 +0300

    saga effects and fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useSelector, useDispatch} from "react-redux";
-import {increaseCount, decreaseCount} from "./redux/actions/actionCreator";
+import {increaseCount, decreaseCount, getLatestNews} from "./redux/actions/actionCreator";
 
 // https://react-redux.js.org/using-react-redux/usage-with-typescript#typing-the-useselector-hook
 type StoreProps = {
@@ -24,12 +24,18 @@ const App = () => {
         dispatch(decreaseCount())
     }
 
+    const handleNews = () => {
+        // setCount(count - 1)
+        dispatch(getLatestNews())
+    }
+
     return (
         <div>
             <h1>HELLO REDUX-SAGA</h1>
             <h1>Count: {count}</h1>
             <button onClick={handleIncrease}>Increase</button>
             <button onClick={handleDecrease}>Decrease</button>
+            <button onClick={handleNews}>Get News</button>
         </div>
     );
 };
--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,4 +1,31 @@
+import {take, takeEvery, takeLatest, takeLeading, select} from '@redux-saga/core/effects';
+import {INCREASE_COUNT, DECREASE_COUNT, GET_LATEST_NEWS} from "../constants";
+import {getLatestNews} from '../../api';
+
+// const delay = (time: number) => new Promise((resolve, reject) => {
+//     setTimeout(resolve, time * 1000);
+// })
+
+// all activities for async code and business logic: requests, browser api
+export function* workerSaga(): any{
+    // const count = yield select(({counter}) => counter.count);
+    // yield delay(2);
+    // console.log(`request: ${count}`);
+    const data = yield getLatestNews();
+    console.log('data', data)
+}
+// tracking the action, witch is triggered in app
+export function* watchClickSaga(){
+    // yield take(INCREASE_COUNT);
+    // console.log('HELLO, 1!')
+    // yield take(DECREASE_COUNT);
+    // console.log('HELLO, 2!')
+    // yield takeEvery(INCREASE_COUNT, workerSaga);
+    // yield takeLatest(INCREASE_COUNT, workerSaga);
+    // yield takeLeading(INCREASE_COUNT, workerSaga);
+    yield takeEvery(GET_LATEST_NEWS, workerSaga);
+}
+
 export default function* rootSaga(){
-    console.log('HELLO, SAGA!')
-    yield;
+    yield watchClickSaga();
 }
