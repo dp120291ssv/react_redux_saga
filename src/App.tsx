0b73623b3b7bfc0aef519153107dee7bfd4a8211
Semenov Sergei <dp120291ssv@gmail.com>
2022-10-25 18:57:15 +0300

    prepare for parallel tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {useSelector, useDispatch} from "react-redux";
 import {increaseCount, decreaseCount, getLatestNews} from "./redux/actions/actionCreator";
+import store from "./redux/store";
+import News from "./components/news/news";
 
 // https://react-redux.js.org/using-react-redux/usage-with-typescript#typing-the-useselector-hook
 type StoreProps = {
@@ -8,10 +10,15 @@ type StoreProps = {
 }
 
 const App = () => {
+    // @ts-ignore
+    const latestNews = useSelector(store => store?.news?.latestNews || [])
+    // @ts-ignore
+    const popularNews = useSelector(store => store?.news?.popularNews || [])
+    const dispatch = useDispatch();
+
     // const store = useSelector(store => store);
     // console.log('store', store)
     const count = useSelector((store: StoreProps)  => store?.counter?.count || 0);
-    const dispatch = useDispatch();
     // const [count, setCount] = useState(0)
 
     const handleIncrease = () => {
@@ -30,13 +37,20 @@ const App = () => {
     }
 
     return (
+        <>
         <div>
             <h1>HELLO REDUX-SAGA</h1>
             <h1>Count: {count}</h1>
             <button onClick={handleIncrease}>Increase</button>
             <button onClick={handleDecrease}>Decrease</button>
-            <button onClick={handleNews}>Get News</button>
         </div>
+            <div>
+                <hr/>
+                <button onClick={handleNews}>Get News</button>
+                <News news={latestNews} title="Latest News" />
+                <News news={popularNews} title="Popular News" />
+            </div>
+        </>
     );
 };
 
